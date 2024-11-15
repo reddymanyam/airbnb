b6c056ef24616c700a194514c2c93ed6c6ba1dbf
reddymanyam <reddappareddy18@gmail.com>
2024-11-15 17:09:33 +0530

    cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,6 +3,8 @@ import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@
 import Grid from '@mui/material/Grid2';
 import {locations} from '../../data/mockdata';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Cards = () => {
     return (
@@ -11,9 +13,11 @@ const Cards = () => {
                 {locations.map((location) => (
                     <Grid item size={{ xs: 12, sm: 6, md: 4, lg: 3 }} >
                     <Card sx={{ borderRadius: "12px" }} elevation={1}>
-                        <CardActionArea>
+                        <CardActionArea sx={{'&:hover .hoverIcons':{backgroundColor:"white", borderRadius:"50%",opacity:1}}}>
                             <CardMedia component="img" height="260px" image={location.locationImages.map((img)=>(img.url))} alt='resart' />
                             <FavoriteBorderIcon sx={{position:"absolute", top:"8px", right:"8px"}} />
+                            <KeyboardArrowLeftIcon className='hoverIcons' sx={{position:"absolute", bottom:"61%", left:"5px"}} />
+                            <KeyboardArrowRightIcon className='hoverIcons' sx={{position:"absolute", bottom:"61%", right:"5px"}} />
                             <CardContent >
                                 <Typography variant='body1' component="div" >{location.location}</Typography>
                                 <Typography variant='body1' sx={{ color: "gray" }}>{location.days}</Typography>
@@ -31,3 +35,4 @@ const Cards = () => {
 }
 
 export default Cards
+
