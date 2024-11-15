9f78499e49c124b5e6a048f9980a38ed24cd7ac0
reddymanyam <reddappareddy18@gmail.com>
2024-11-15 11:05:40 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Options from './components/Options/Options'
 
 const App = () => {
   return (
     <>
     <Navbar />
+    <Options />
     </>
   )
 }
