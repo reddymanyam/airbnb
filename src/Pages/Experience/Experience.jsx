28decaf0f94ae26d31d4f5ddc98c5e8d1944f8f5
reddymanyam <reddappareddy18@gmail.com>
2024-11-16 13:35:31 +0530

    experience

---
+++ src/Pages/Experience/Experience.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div>
+      <Typography>Experience...</Typography>
+    </div>
+  )
+}
+
+export default Experience
