90351aa70dfd115f8777b841411ead7e5cc256e4
reddymanyam <reddappareddy18@gmail.com>
2024-12-01 23:41:23 +0530

    Router

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,27 @@
-import { HelpCenter } from "@mui/icons-material";
 import Auth from "../Auth/Auth";
+import Layout from "../components/Layout/Layout";
+import MainLayout from "../components/MainLayout/MainLayout/MainLayout";
+import Dashboard from "../Pages/Dashboard/Dashboard";
 import Experience from "../Pages/Experience/Experience";
+import HelpCenter from "../Pages/Help/HelpCenter";
+import HomePage from "../Pages/Home/HomePage";
 
-const Router =[
-
-    {path:"/", element:<Auth />},
-    {path:"/home", element:<Home />},
-    {path:"/experience", element:<Experience />},
-    {path:"/help", element:<HelpCenter />}
+const Router = [
+    {
+        path:"/",
+        element:<MainLayout />,
+        children:[
+            {path:'/', element:<Dashboard />}
+        ]
+    },
+    {
+        path : "/",
+        element:<Layout />,
+        children:[
+            {path:'/', element:<Auth />},
+            {path:'/homepage', element:<HomePage />},
+            {path:'/helpcenter', element:<HelpCenter />},
+            {path:'/experience', element:<Experience />},
+        ]
+    }
 ]
