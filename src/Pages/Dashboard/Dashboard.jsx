ba5a83c0df63090e4aabee24c043e1a4f524c729
reddymanyam <reddappareddy18@gmail.com>
2024-12-01 23:22:26 +0530

    Dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Options from '../Options/Options'
+import Cards from '../Cards/Cards'
 
 const Dashboard = () => {
   return (
     <>
-        
+        <Navbar />
+        <Options />
+        <Cards />
     </>
   )
 }
