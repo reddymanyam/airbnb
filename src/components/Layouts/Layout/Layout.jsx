3573bcd552d46ea0b722d5f6f70a2075f9b49c2e
reddymanyam <reddappareddy18@gmail.com>
2024-12-01 23:15:45 +0530

    Layout

--- src/components/Layouts/Layout/Layout.jsx
+++ src/components/Layouts/Layout/Layout.jsx
@@ -1,8 +1,19 @@
+import { Stack } from '@mui/material'
 import React from 'react'
+import Auth from '../../../Auth/Auth';
+import HomePage from '../../../Pages/Home/HomePage';
+import Experience from '../../../Pages/Experience/Experience';
+import HelpCenter from '../../../Pages/Help/HelpCenter';
+
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <Stack>
+        <Auth />
+        <HelpCenter />
+        <Experience />
+        <HomePage />
+    </Stack>
   )
 }
 
