235d0997a445a18a9cd69a910318000c272c68b0
reddymanyam <reddappareddy18@gmail.com>
2024-12-02 00:04:44 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { useRoutes } from 'react-router-dom';
 import Routes from './Auth/Auth';
 
 const App = () => {
-
   const routing = useRoutes(Routes);
+  
   return (
     <>
       {routing}
