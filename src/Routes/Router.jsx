b48ede3aee4e8c49139e6d5d33782e09032536b0
reddymanyam <reddappareddy18@gmail.com>
2024-12-03 11:33:19 +0530

    Router.jsx

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,6 @@
 import Auth from "../Auth/Auth";
 import Layout from "../components/Layout/Layout";
-import MainLayout from "../components/MainLayout/MainLayout/MainLayout";
+import MainLayout from "../components/MainLayout/MainLayout";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import Experience from "../Pages/Experience/Experience";
 import HelpCenter from "../Pages/Help/HelpCenter";
@@ -11,18 +11,17 @@ const Router = [
         path:"/",
         element:<MainLayout />,
         children:[
-            {path:'/', element:<Dashboard />},
-            { path: '*', element: <Navigate to="/" replace /> }
+            {path:'/', element:<Dashboard />}
         ]
     },
     {
-        path : "/auth",
+        path : "/",
         element:<Layout />,
         children:[
             {path:'/auth', element:<Auth />},
-            {path:'/auth/homepage', element:<HomePage />},
-            {path:'/auth/helpcenter', element:<HelpCenter />},
-            {path:'/auth/experience', element:<Experience />},
+            {path:'/homepage', element:<HomePage />},
+            {path:'/helpcenter', element:<HelpCenter />},
+            {path:'/experience', element:<Experience />},
         ]
     }
 ]
