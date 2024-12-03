dd0858726d12b45cb981a96379079b652bcd6391
reddymanyam <reddappareddy18@gmail.com>
2024-12-03 18:21:56 +0530

    Homepage

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,11 +1,20 @@
-import { Typography } from '@mui/material'
+import { Box, Button, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { SiAirbnb } from 'react-icons/si'
+import { RxHome } from "react-icons/rx";
 
 const HomePage = () => {
+
   return (
-    <div>
-      <Typography>Home page...</Typography>
-    </div>
+    <Box sx={{width:"100%", minHeight:"100vh"}}>
+        <Stack sx={{width:"91%", height:"80px", background:"white", mx:"auto", flexDirection:"row", justifyContent:"space-between", alignItems:"center"}}>
+        <SiAirbnb size={36} color='#ff385c' />
+        <Stack sx={{flexDirection:"row", gap:"30px", alignItems:"center"}}>  
+          <Typography color='black' variant='body1' component="h2" sx={{fontWeight:500}}> Ready to Airbnb it?</Typography>
+          <Button sx={{bgcolor:'#d90166', borderRadius:"15px", minWidth:"200px"}} variant='contained'><RxHome size={28} /> <Typography color='white' variant='h6' component="h2" sx={{fontWeight:500, fontSize:"20px", marginLeft:"10px", textTransform: "none"}}> Airbnb Setup</Typography></Button>
+        </Stack>
+        </Stack>
+    </Box>
   )
 }
 
