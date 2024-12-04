c550634d72b36679aa4f970a3d6767dc238f5a51
reddymanyam <reddappareddy18@gmail.com>
2024-12-04 16:50:41 +0530

    HomePage

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -4,6 +4,7 @@ import { SiAirbnb } from 'react-icons/si'
 import { RxHome } from "react-icons/rx";
 import { useNavigate } from 'react-router-dom';
 import GoogleMaps from '../../GoogleMaps/GoogleMaps';
+import Grid from '@mui/material/Grid2';
 
 const HomePage = () => {
 
@@ -27,8 +28,22 @@ const HomePage = () => {
       <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", margin: "15px auto" }}>
         <Typography variant='h4' component="h2" sx={{ fontWeight: "600", color: "light black" }}>Airbnb it easily with Airbnb Setup</Typography>
       </Box>
-      <Box sx={{ width: "85%", display: 'flex', justifyContent: "center", margin: "30px auto" }}>
-        <img src='https://a0.muscache.com/im/pictures/65214d06-ffb4-4b70-93c0-01d368e76649.jpg?im_w=2560&im_q=highq' alt='imghome' style={{ width: "100%" }} />
+      <Box sx={{ width: "85%", margin: "20px auto" }}>
+        <img src='https://a0.muscache.com/im/pictures/65214d06-ffb4-4b70-93c0-01d368e76649.jpg?im_w=2560&im_q=highq' alt='imghome' style={{ width: "100%", marginBottom:"50px" }} />
+        <Grid container spacing={2}>
+        <Grid size={{xs:12, sm:6, md:4}}>
+          <Typography variant='h6' component="h2">One-to-one guidance from a Superhost</Typography>
+          <Typography variant='subtitle1' component="h2" color='gray'>We’ll match you with a Superhost in your area, who’ll guide you from your first question to your first guest – by phone, video call or chat.</Typography>
+        </Grid>
+        <Grid size={{xs:12, sm:6, md:4}}>
+        <Typography variant='h6' component="h2">An experienced guest for your first booking</Typography>
+          <Typography variant='subtitle1' component="h2" color='gray'>For your first booking, you can choose to welcome an experienced guest who has at least three stays and a good track record on Airbnb.</Typography>
+        </Grid>
+        <Grid size={{xs:12, sm:6, md:4}}>
+        <Typography variant='h6' component="h2">Specialised support from Airbnb</Typography>
+        <Typography variant='subtitle1' component="h2" color='gray'>New Hosts get one-tap access to specially trained Community Support agents who can help with everything from account issues to billing support.</Typography>
+        </Grid>
+        </Grid>
       </Box>
     </Box>
   )
