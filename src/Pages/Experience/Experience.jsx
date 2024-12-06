04f77e5ff357cb8ab75f965d3e16f9fa643439a7
reddymanyam <reddappareddy18@gmail.com>
2024-12-06 15:46:58 +0530

    Experience

--- src/Pages/Experience/Experience.jsx
+++ src/Pages/Experience/Experience.jsx
@@ -1,12 +1,28 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Box, Button, Stack, Typography } from '@mui/material';
 
 const Experience = () => {
   return (
-    <div>
-      <Typography>Experience...</Typography>
-    </div>
-  )
-}
+    <Box sx={{ bgcolor: "black" }}>
+      <Stack sx={{ width: "100%",  bgcolor: "white", justifyContent: "center", alignItems: "center", padding: "60px 0",  }}>
+        <Stack sx={{ width: "80%", alignItems: "flex-start", justifyContent: 'left', gap: '40px' }}>
+        <Typography variant='h5'>Host an experience on Airbnb</Typography>
+      <Typography variant='h2' font>Earn money leading people on activities you love.</Typography>
+      <Stack sx={{}}> 
+       <Button variant='contained' sx={{bgcolor:"black", padding:"15px", borderRadius:"15px"}}>Lets Go</Button>
 
-export default Experience
+      </Stack>
+        </Stack>
+      </Stack>
+
+      <Stack sx={{color:"white"}}>
+      <Typography variant='h5'>Host an experience on Airbnb Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam explicabo dolorem consequatur quia! Maxime consectetur, aliquam blanditiis ex porro exercitationem quo harum sunt aut sint reiciendis qui, dolor ipsum repellendus!</Typography>
+      <Typography variant='h1'>Earn money leading people on activities you love. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, id assumenda! Natus iste tenetur at modi ea consequatur delectus laborum, numquam quidem vero tempora, neque ex fugiat odio quaerat voluptas?</Typography>
+      </Stack>
+
+
+    </Box>
+  );
+};
+
+export default Experience;
