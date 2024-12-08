c7edcde0b591d33c67603b94f0f4460ef58148b1
reddymanyam <reddappareddy18@gmail.com>
2024-12-08 22:02:18 +0530

    Card

--- src/Pages/Cards/Cards.jsx
+++ src/Pages/Cards/Cards.jsx
@@ -1,72 +1,103 @@
 import React, { useState } from 'react';
 import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid2';
-import { locations } from '../../data/mockdata';
+import Grid from '@mui/material/Grid';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { useTheme } from '@mui/material/styles';
+import { locations } from '../../data/mockdata';
 
 const Cards = () => {
+  const [currentIndices, setCurrentIndices] = useState(locations.map(() => 0));
+  const theme = useTheme(); // Access theme values
 
-    const [currentIndices, setCurrentIndices] = useState(
-        locations.map(() => 0) 
-    );
+  const goToPrevious = (index) => {
+    setCurrentIndices((prevIndices) => {
+      const newIndices = [...prevIndices];
+      const isFirstSlide = newIndices[index] === 0;
+      newIndices[index] = isFirstSlide ? locations[index].locationImages.length - 1 : newIndices[index] - 1;
+      return newIndices;
+    });
+  };
 
-    const goToPrevious = (index) => {
-        setCurrentIndices((prevIndices) => {
-            const newIndices = [...prevIndices];
-            const isFirstSlide = newIndices[index] === 0;
-            newIndices[index] = isFirstSlide ? locations[index].locationImages.length - 1 : newIndices[index] - 1;
-            return newIndices;
-        });
-    };
+  const goToNext = (index) => {
+    setCurrentIndices((prevIndices) => {
+      const newIndices = [...prevIndices];
+      const isLastSlide = newIndices[index] === locations[index].locationImages.length - 1;
+      newIndices[index] = isLastSlide ? 0 : newIndices[index] + 1;
+      return newIndices;
+    });
+  };
 
-    const goToNext = (index) => {
-        setCurrentIndices((prevIndices) => {
-            const newIndices = [...prevIndices];
-            const isLastSlide = newIndices[index] === locations[index].locationImages.length - 1;
-            newIndices[index] = isLastSlide ? 0 : newIndices[index] + 1;
-            return newIndices;
-        });
-    };
-
-    return (
-        <Box sx={{ width: "100%", boxSizing: "border-box" }}>
-            <Grid container spacing={2} sx={{ padding: { xs: "2px 18px", md: "3px 40px 15px 40px", lg: "3px 40px 15px 40px" } }}>
-                {locations.map((location, index) => (
-                    <Grid item key={index} size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
-                        <Card sx={{ borderRadius: "12px" }} elevation={1}  >
-                            <CardActionArea disableRipple sx={{ '&:hover .hoverIcons': { backgroundColor: "white", borderRadius: "50%", opacity: 1 } }}>
-                                <CardMedia
-                                    component="img"
-                                    height="260px"
-                                    image={location.locationImages[currentIndices[index]].url}
-                                    alt="carousel"
-                                />
-                                <FavoriteBorderIcon sx={{ position: "absolute", top: "8px", right: "8px" }} />
-                                <KeyboardArrowLeftIcon
-                                    className="hoverIcons"
-                                    sx={{ position: "absolute", bottom: "61%", left: "5px", cursor: "pointer" }}
-                                    onClick={() => goToPrevious(index)}
-                                />
-                                <KeyboardArrowRightIcon
-                                    className="hoverIcons"
-                                    sx={{ position: "absolute", bottom: "61%", right: "5px", cursor: "pointer" }}
-                                    onClick={() => goToNext(index)}
-                                />
-                                <CardContent>
-                                    <Typography variant="body1" component="div">{location.location}</Typography>
-                                    <Typography variant="body1" sx={{ color: "gray" }}>{location.days}</Typography>
-                                    <Typography variant="body1" sx={{ color: "gray" }} gutterBottom>{location.price}</Typography>
-                                    <Typography variant="body1">{location.rating}*</Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box sx={{ width: '100%', boxSizing: 'border-box' }}>
+      <Grid container spacing={2} sx={{ padding: theme.spacing(2, 5) }}>
+        {locations.map((location, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+            <Card elevation={2}>
+              <CardActionArea
+                disableRipple
+                sx={{
+                  '&:hover .hoverIcons': {
+                    backgroundColor: theme.palette.background.paper,
+                    borderRadius: '50%',
+                    opacity: 1,
+                  },
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="260px"
+                  image={location.locationImages[currentIndices[index]].url}
+                  alt={location.location}
+                />
+                <FavoriteBorderIcon
+                  sx={{
+                    position: 'absolute',
+                    top: theme.spacing(1),
+                    right: theme.spacing(1),
+                    color: theme.palette.primary.main,
+                  }}
+                />
+                <KeyboardArrowLeftIcon
+                  className="hoverIcons"
+                  sx={{
+                    position: 'absolute',
+                    bottom: '50%',
+                    left: theme.spacing(1),
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => goToPrevious(index)}
+                />
+                <KeyboardArrowRightIcon
+                  className="hoverIcons"
+                  sx={{
+                    position: 'absolute',
+                    bottom: '50%',
+                    right: theme.spacing(1),
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => goToNext(index)}
+                />
+                <CardContent>
+                  <Typography variant="body1" component="div">
+                    {location.location}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    {location.days}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary" gutterBottom>
+                    {location.price}
+                  </Typography>
+                  <Typography variant="body1">{location.rating}*</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Cards;
