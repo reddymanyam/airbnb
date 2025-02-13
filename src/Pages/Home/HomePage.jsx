ee7869ec01cbd234bcf0a92ba5776f7ab474aa7f
reddymanyam <reddappareddy18@gmail.com>
2025-02-13 14:22:13 +0530

    Home page

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -6,23 +6,26 @@ import { useNavigate } from 'react-router-dom';
 import GoogleMaps from '../../GoogleMaps/GoogleMaps';
 import Grid from '@mui/material/Grid2';
 import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
 
 const HomePage = () => {
-
+ 
   const navigate = useNavigate();
 
   const handleHome = () => {
     navigate('/')
   }
+
   return (
     <Box sx={{ width: "100%", minHeight: "100vh", background: "#fff" }}>
-      <Stack sx={{ width: "91%", height: "80px", background: "white", mx: "auto", flexDirection: "row", justifyContent: "space-between", alignItems: "center", position: "sticky", top: 0, zIndex: 1000 }}>
+      <Navbar variant="homepage" />
+      {/* <Stack sx={{ width: "91%", height: "80px", background: "white", mx: "auto", flexDirection: "row", justifyContent: "space-between", alignItems: "center", position: "sticky", top: 0, zIndex: 1000 }}>
         <SiAirbnb size={36} color='#ff385c' onClick={handleHome} cursor="pointer" />
         <Stack sx={{ flexDirection: "row", gap: "30px", alignItems: "center" }}>
           <Typography color='black' variant='body1' component="h2" sx={{ fontWeight: 500 }}> Ready to Airbnb it?</Typography>
           <Button sx={{ bgcolor: '#d90166', borderRadius: "15px", minWidth: "200px" }} variant='contained'><RxHome size={28} /> <Typography color='white' variant='h6' component="h2" sx={{ fontWeight: 500, fontSize: "20px", marginLeft: "10px", textTransform: "none", cursor: "pointer" }}> Airbnb Setup</Typography></Button>
         </Stack>
-      </Stack>
+      </Stack> */}
       <Box sx={{ display: "flex", justifyContent: "center", margin: "10px 0" }}>
         <GoogleMaps />
       </Box>
