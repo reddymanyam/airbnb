54d223fb1fbd49b87205d2b31579767e8c29953a
reddymanyam <reddappareddy18@gmail.com>
2025-02-16 22:49:45 +0530

    update: HelpCenter updated

--- src/Pages/Help/HelpCenter.jsx
+++ src/Pages/Help/HelpCenter.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, InputAdornment, TextField, Typography } from '@mui/material'
+import { Box, Button, IconButton, InputAdornment, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { IoSearch } from 'react-icons/io5'
 import Navbar from '../Navbar/Navbar';
@@ -8,7 +8,7 @@ const HelpCenter = () => {
   return (
     <>
       <Box sx={{ width: "100%", height: "100vh", background: "white" }}>
-       <Navbar variant="helpcenter"/>
+        <Navbar variant="helpcenter" />
         <Box sx={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", padding: "50px" }}>
           <Typography variant="h3" component="h2">Hi, how can we help?</Typography>
           <Box sx={{ width: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "10px" }}>
@@ -41,6 +41,16 @@ const HelpCenter = () => {
               }} />
           </Box>
         </Box>
+        <Stack>
+
+          <Stack sx={{ padding: "20px", border: "1px solid gray", borderRadius: "20px", width: "85%", margin: "0 auto", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+            <Box>
+              <Typography variant='h4' component="h2">We’re here for you</Typography>
+              <Typography variant='subtitle1' component="h2">Log in to get help with your reservations, account, and more.</Typography>
+            </Box>
+            <Button variant='contained' sx={{ backgroundColor: '#ff385c', padding: "12px 120px", borderRadius: "8px" }}>Login or SignUp</Button>
+          </Stack>
+        </Stack>
       </Box>
       <Footer />
     </>
