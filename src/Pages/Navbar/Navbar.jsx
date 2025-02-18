0aa82b9fc1a21c0b4bb86f5a2508912186dff26f
reddymanyam <reddappareddy18@gmail.com>
2025-02-18 17:37:19 +0530

    update: Navbar is updated

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -8,7 +8,8 @@ import {
   Stack,
   Typography,
   Menu,
-  MenuItem
+  MenuItem,
+  useMediaQuery
 } from '@mui/material';
 import { SiAirbnb } from "react-icons/si";
 import { CiGlobe } from "react-icons/ci";
@@ -21,6 +22,9 @@ const Navbar = ({ variant = "default" }) => {
   const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
+  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isDesktop = useMediaQuery('(min-width: 769px)');
 
   const handleClick = (event) => setAnchorEl(event.currentTarget);
   const handleClose = () => setAnchorEl(null);
@@ -40,7 +44,7 @@ const Navbar = ({ variant = "default" }) => {
       alignItems: "center"
     }}>
       <Box sx={{
-        width: variant === "homepage" ? "91%" : "95%",
+        width: "95%",
         display: "flex",
         margin: "0 auto",
         justifyContent: "space-between",
@@ -49,103 +53,71 @@ const Navbar = ({ variant = "default" }) => {
         {/* Airbnb Logo */}
         <SiAirbnb size={36} color='#ff385c' onClick={handleHome} cursor="pointer" />
 
-        {variant === "homepage" ? (
-          // Homepage Variant
-          <Stack sx={{ flexDirection: "row", gap: "30px", alignItems: "center" }}>
-            <Typography color='black' variant='body1' sx={{ fontWeight: 500 }}>
-              Ready to Airbnb it?
-            </Typography>
-            <Button sx={{ bgcolor: '#d90166', borderRadius: "15px", minWidth: "200px" }} variant='contained'>
-              <RxHome size={28} />
-              <Typography color='white' variant='h6'
-                sx={{ fontWeight: 500, fontSize: "20px", marginLeft: "10px", textTransform: "none" }}>
-                Airbnb Setup
-              </Typography>
-            </Button>
-          </Stack>
-        ) : variant === "helpcenter" ? (
-          // HelpCenter Variant - Only shows right side menu
-          <Box sx={{ display: "flex", alignItems: "center", gap: 2, marginLeft: "auto" }}>
+        {/* Responsive Search Bar */}
+        {isDesktop && variant !== "helpcenter" && (
+          <Paper elevation={1} sx={{
+            border: "1px solid #E5E4E2",
+            borderRadius: "50px",
+            padding: "4px 3px 4px 26px",
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
+            cursor: "pointer"
+          }}>
+            <Typography variant='body1' color='black'>Anywhere</Typography>
+            <Divider orientation="vertical" variant="middle" flexItem />
+            <Typography variant='body1' color='black'>Any week</Typography>
+            <Divider orientation="vertical" variant="middle" flexItem />
+            <Typography variant='body1' color='gray'>Add guests</Typography>
+            <IconButton sx={{
+              color: "white",
+              backgroundColor: "#ff385c",
+              height: "35px",
+              width: "35px",
+              '&:hover': { backgroundColor: '#ff385c' }
+            }}>
+              <IoSearch />
+            </IconButton>
+          </Paper>
+        )}
+
+        {/* Right Side Menu */}
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          {isDesktop && (
             <Typography variant='body1' color='#000000' sx={{ cursor: "pointer" }}
               onClick={() => handleNavigation('/homepage')}>
               Airbnb your home
             </Typography>
-            <IconButton sx={{ fontSize: '20px', color: 'black' }}>
-              <CiGlobe />
-            </IconButton>
-            <Paper sx={{
-              border: "1px solid #E5E4E2",
-              borderRadius: "50px",
-              padding: "2px"
-            }}>
-              <Stack sx={{ display: "flex", flexDirection: "row" }}>
-                <IconButton onClick={handleClick}>
-                  <IoMenuOutline color='black' />
-                </IconButton>
-                <IconButton onClick={handleClick}>
-                  <AccountCircleTwoToneIcon />
-                </IconButton>
-              </Stack>
-            </Paper>
-          </Box>
-        ) : (
-          // Default Variant
-          <>
-            {/* Search Bar */}
-            <Paper elevation={1} sx={{
-              border: "1px solid #E5E4E2",
-              borderRadius: "50px",
-              padding: "4px 3px 4px 26px",
-              display: "flex",
-              alignItems: "center",
-              gap: 1,
-              cursor: "pointer"
-            }}>
-              <Typography variant='body1' color='black'>Anywhere</Typography>
-              <Divider orientation="vertical" variant="middle" flexItem />
-              <Typography variant='body1' color='black'>Any week</Typography>
-              <Divider orientation="vertical" variant="middle" flexItem />
-              <Typography variant='body1' color='gray'>Add guests</Typography>
-              <IconButton sx={{
-                color: "white",
-                backgroundColor: "#ff385c",
-                height: "35px",
-                width: "35px",
-                '&:hover': { backgroundColor: '#ff385c' }
-              }}>
-                <IoSearch />
-              </IconButton>
-            </Paper>
+          )}
 
-            {/* Right Side Menu */}
-            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-              <Typography variant='body1' color='#000000' sx={{ cursor: "pointer" }}
-                onClick={() => handleNavigation('/homepage')}>
-                Airbnb your home
-              </Typography>
-              <IconButton sx={{ fontSize: '20px', color: 'black' }}>
-                <CiGlobe />
+          <IconButton sx={{ fontSize: '20px', color: 'black' }}>
+            <CiGlobe />
+          </IconButton>
+
+          <Paper sx={{
+            border: "1px solid #E5E4E2",
+            borderRadius: "50px",
+            padding: "2px"
+          }}>
+            <Stack sx={{ display: "flex", flexDirection: "row" }}>
+              <IconButton onClick={handleClick}>
+                <IoMenuOutline color='black' />
               </IconButton>
-              <Paper sx={{
-                border: "1px solid #E5E4E2",
-                borderRadius: "50px",
-                padding: "2px"
-              }}>
-                <Stack sx={{ display: "flex", flexDirection: "row" }}>
-                  <IconButton onClick={handleClick}>
-                    <IoMenuOutline color='black' />
-                  </IconButton>
-                  <IconButton onClick={handleClick}>
-                    <AccountCircleTwoToneIcon />
-                  </IconButton>
-                </Stack>
-              </Paper>
-            </Box>
-          </>
-        )}
+              <IconButton onClick={handleClick}>
+                <AccountCircleTwoToneIcon />
+              </IconButton>
+            </Stack>
+          </Paper>
+        </Box>
 
         {/* Account Menu */}
         <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+          {isMobile && (
+            <>
+              <MenuItem onClick={() => handleNavigation('/search')}>Search</MenuItem>
+              <Divider />
+            </>
+          )}
           <MenuItem onClick={() => handleNavigation('/auth')}>Sign up</MenuItem>
           <MenuItem onClick={() => handleNavigation('/auth')}>Login</MenuItem>
           <Divider />
