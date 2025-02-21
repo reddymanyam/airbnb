0d92de545e98fde4ab5479541d8af030202ef5df
reddymanyam <reddappareddy18@gmail.com>
2025-02-21 23:37:10 +0530

    update: HelpCenter updated

--- src/Pages/Help/HelpCenter.jsx
+++ src/Pages/Help/HelpCenter.jsx
@@ -3,7 +3,49 @@ import React from 'react'
 import { IoSearch } from 'react-icons/io5'
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+
+
 const HelpCenter = () => {
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <>
@@ -43,13 +85,35 @@ const HelpCenter = () => {
         </Box>
         <Stack>
 
-          <Stack sx={{ padding: "20px", border: "1px solid gray", borderRadius: "20px", width: "85%", margin: "0 auto", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+          <Box sx={{ width: '80%', margin:"0 auto" }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="Guest" {...a11yProps(0)} />
+                <Tab label="Host" {...a11yProps(1)} />
+                <Tab label="Experience Host" {...a11yProps(2)} />
+                <Tab label="Travel admin" {...a11yProps(3)} />
+              </Tabs>
+            </Box>
+
+          <Stack sx={{ padding: "20px", border: "1px solid gray", borderRadius: "20px", width: "85%", margin: "40px auto", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
             <Box>
               <Typography variant='h4' component="h2">We’re here for you</Typography>
               <Typography variant='subtitle1' component="h2">Log in to get help with your reservations, account, and more.</Typography>
             </Box>
-            <Button variant='contained' sx={{ backgroundColor: '#ff385c', padding: "12px 120px", borderRadius: "8px" }}>Login or SignUp</Button>
+            <Button variant='contained' sx={{ backgroundColor: '#dc0e63', padding: "12px 120px", borderRadius: "8px" }}>Login or SignUp</Button>
           </Stack>
+            <CustomTabPanel value={value} index={0}>
+              Item One
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              Item Two
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+              Item Three
+            </CustomTabPanel>
+          </Box>
+
+
         </Stack>
       </Box>
       <Footer />
