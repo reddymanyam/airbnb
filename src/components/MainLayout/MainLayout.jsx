7c0b6878a67ae9740b783fea4ed0a51d34b8c16c
reddymanyam <reddappareddy18@gmail.com>
2025-03-15 13:35:18 +0530

    updated: Main Layout is updated

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Dashboard from '../../Pages/Dashboard/Dashboard'
+import Auth from '../../Auth/Auth'
 
 const MainLayout = () => {
   return (
     <>
-        <Dashboard />
+        {/* <Dashboard /> */}
+        <Auth />
     </>
   )
 }
