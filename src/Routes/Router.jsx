9390e6864aed8a4844aba272717a8ab4f590d56f
reddymanyam <reddappareddy18@gmail.com>
2025-03-15 13:56:48 +0530

    Updated: Router is Updated

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import React, { lazy, Suspense } from "react";
+import ProtectedRoute from "./ProtectedRoute";
 
 const Auth = lazy(() => import("../Auth/Auth"));
 const Layout = lazy(() => import("../components/Layout/Layout"));
@@ -33,7 +34,7 @@ const Router = [
         path: "/",
         element: (
             <Suspense fallback={<Typography variant="h2" componetnt="h2">Loading...</Typography>}>
-                <Layout />
+                <ProtectedRoute componet={Layout} />
             </Suspense>
         ),
         children: [
