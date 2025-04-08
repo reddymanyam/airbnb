dd5eec93ae85ce034e66614922495c6990bd74e3
Reddy Manyam <142713295+reddymanyam@users.noreply.github.com>
2025-04-08 23:21:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,1 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
 
-const PORT = process.env.PORT;
-
-
-
-app.listen(()=>{
-    console.log(`The server is running at ${PORT}`)
-})
