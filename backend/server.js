5338286971d9d99ec765775c053e54a0a234d310
reddymanyam <reddappareddy18@gmail.com>
2025-04-11 23:55:27 +0530

    server is updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 
+// index.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic GET route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
