5f17bd54466148b8fb978e2d8b403ac184820e8e
reddymanyam <reddappareddy18@gmail.com>
2025-04-12 23:37:56 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,10 @@
-
-// index.js
-
 const express = require('express');
 const app = express();
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Basic GET route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running at port ${PORT}`);
 });
