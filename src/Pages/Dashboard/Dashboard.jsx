be8c0819bee39f26ce391fec842f001fcb5be626
reddymanyam <reddappareddy18@gmail.com>
2025-05-13 20:02:55 +0530

    updated

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Options from '../Options/Options'
 import Cards from '../Cards/Cards'
+import { Box } from '@mui/material';
 
 const Dashboard = () => {
   return (
     <>
-        <Navbar />
-        <Options />
-        <Cards />
+      <Navbar />
+      <Options />
+      <Cards />
     </>
   )
 }
